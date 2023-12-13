722382ddd9c65be544229a9991d46f835b23260c
Ravitejareddy-s <72079785+Ravitejareddy-s@users.noreply.github.com>
2023-12-13 08:47:10 +0000

    check_import_lib

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-# from PyPDF2 import PdfReader
-# import requests
-# from Bio import Entrez
-# import pandas as pd
-# import numpy as np
-# from requests_html import HTMLSession
-# from requests.exceptions import ConnectionError
-# import xml.etree.ElementTree as ET
-# from pathlib import Path
-# import re
-# from metapub import PubMedFetcher
+from PyPDF2 import PdfReader
+import requests
+from Bio import Entrez
+import pandas as pd
+import numpy as np
+from requests_html import HTMLSession
+from requests.exceptions import ConnectionError
+import xml.etree.ElementTree as ET
+from pathlib import Path
+import re
+from metapub import PubMedFetcher
 
-# import random
-# import json
+import random
+import json
 from datetime import datetime
 
 
