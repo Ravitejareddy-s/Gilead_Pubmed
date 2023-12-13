a524dce1ee9164b82c69bb0a5d175c6e29156f77
Ravitejareddy-s <72079785+Ravitejareddy-s@users.noreply.github.com>
2023-12-13 11:42:48 +0000

    added stetup.sh file

---
+++ setup.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt install -y python3 python3-pip
+pip3 install --upgrade pip
+cd /path/to/your/requirements
+pip3 install -r requirements.txt
+
+# chmod +x setup.sh
+# ./setup.sh
